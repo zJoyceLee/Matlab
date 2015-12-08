dd38fd4ba071848a62690b68b1c762910c799cc1
zJoyceLee <zJoyceLee@yeah.net>
2015-12-08 22:28:58 +0800

    homework3, gauss elimination.

---
+++ gauss.m
@@ -0,0 +1,10 @@
+function [x, det, flag] = Gauss(A, b)
+% A is coefficient matrix
+% b is constant matrix
+% x is the solution of equations
+% det is the determinant of A
+% flag: True or False
+[n, m] = size(A);
+nb = length(b);
+
+endfunction
